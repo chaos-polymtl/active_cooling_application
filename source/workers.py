2da40dc71a8d604fc788fbf798b31e7640339dd0
Wieb-ke <wiebke.mainville@polymtl.ca>
2025-11-13 14:19:47 -0500

    add pring for Exception in stop function

--- source/workers.py
+++ source/workers.py
@@ -12,7 +12,6 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
-import traceback
 from PySide6.QtCore import QObject, QTimer, QElapsedTimer, Signal, Slot
 import numpy as np
 
@@ -157,10 +156,9 @@ class MeasureAndControlWorker(QObject):
         try:
             if self.timer.isActive():
                 self.timer.stop()
-        except Exception:
-            print("[Worker.stop] Exception while stopping timer:")
-            traceback.print_exc()
-
+        except Exception as e:
+            print(e)
+            
     def get_time(self):
         self.application.time = self.elapsed_timer.elapsed() / 1000
         self.application.time_step = self.elapsed_timer.elapsed() / 1000 - self.application.previous_time
