536267bc7b5e00318dad3333b91067849aa097f0
Wieb-ke <wiebke.mainville@polymtl.ca>
2025-11-28 11:02:40 -0500

    Revert "remove AdjointSS class and comment output expression in AdjointTransient - still outputting something though"
    This reverts commit 62e8dea4ce6769762fb293eeb958cb0ba38f2f11.

--- source/simulation_model/adjoint_optimizer.py
+++ source/simulation_model/adjoint_optimizer.py
@@ -145,7 +145,7 @@ class AdjointTransient:
                     temp_error_to_output[index] = temp_error_on_face[i]
 
                 # We save the solution at every time step. The first one is the initial condition
-                # self.data_manager.save_vtu(time_manager.current_step, self.finite_difference.T, self.finite_difference.heat_flux, h_coeff_to_output, temp_error_to_output)
+                self.data_manager.save_vtu(time_manager.current_step, self.finite_difference.T, self.finite_difference.heat_flux, h_coeff_to_output, temp_error_to_output)
 
                 # Solve the unsteady direct problem for the current time step
                 self.finite_difference.solve()
@@ -165,8 +165,8 @@ class AdjointTransient:
             for i, index in enumerate(self.finite_difference.boundary.dict_boundary_points[target_face]):
                 temp_error_to_output[index] = temp_error_on_face[i]
 
-            # # save the final solution once we exit the loop. The last h_coeff_to_output is the same as one step before the end of the simulation.
-            # self.data_manager.save_vtu(time_manager.current_step, self.finite_difference.T, self.finite_difference.heat_flux, h_coeff_to_output, temp_error_to_output)
+            # save the final solution once we exit the loop. The last h_coeff_to_output is the same as one step before the end of the simulation.
+            self.data_manager.save_vtu(time_manager.current_step, self.finite_difference.T, self.finite_difference.heat_flux, h_coeff_to_output, temp_error_to_output)
 
             # We go back in time solving the adjoint problem. For that we restart the time manager (going forward in time does not change the equation we solve)
             time_manager = TimeManager(self.params)
@@ -182,7 +182,7 @@ class AdjointTransient:
                 for id in id_h_coefficient_imposed:
                     h_coeff_to_output[id] = self.adjoint.boundary.convective_coefficient[id]
 
-                # self.data_manager.save_vtu(time_step, self.adjoint.lambda_t, self.finite_difference.heat_flux, h_coeff_to_output, file_name_prefix="adjoint_output")
+                self.data_manager.save_vtu(time_step, self.adjoint.lambda_t, self.finite_difference.heat_flux, h_coeff_to_output, file_name_prefix="adjoint_output")
 
                 # Solve the adjoint problem using the direct problem solution
                 self.adjoint.solve(direct_temperature_solutions[time_step-1], self.target_T[time_step-1])
@@ -196,7 +196,7 @@ class AdjointTransient:
                 
 
             # We save the final solution, which is at the end
-            # self.data_manager.save_vtu(time_step, self.adjoint.lambda_t, self.finite_difference.heat_flux, h_coeff_to_output, file_name_prefix="adjoint_output")
+            self.data_manager.save_vtu(time_step, self.adjoint.lambda_t, self.finite_difference.heat_flux, h_coeff_to_output, file_name_prefix="adjoint_output")
 
             # Calculate the gradient of the cost function to minimize using the solution of the adjoint and the direct problem
             gradient = -1/(self.params.thermal_conductivity) * adjoint_temperature_solutions * (direct_temperature_solutions - eval(self.params.T_inf[target_face]))
@@ -221,5 +221,143 @@ class AdjointTransient:
 
         if return_h:
             return np.asarray(h_new, dtype=float)
+
+class AdjointSS:
+    def __init__(self, params, finite_difference, data_manager):
+        self.finite_difference_ss = finite_difference
+        self.params = params
+        self.data_manager = data_manager
+        
+        if hasattr(self.params, 'adjoint_target_array'):
+            self.target_T = self.params.adjoint_target_array
+        else:
+            self.target_T = np.ones_like(self.finite_difference_ss.T) * self.target_T
+
+        self.target_face = self.params.adjoint_target_face
+        self.optimize_face = self.params.adjoint_optimize_face
+
+
+    def run_nonlinear(self):
+        # Adjust adjoint boundary parameters
+        self.params_adjoint = copy.deepcopy(self.params)
+        target_face = self.params_adjoint.adjoint_target_face
+
+        # Setup adjoint loop
+        error = 1e6
+        error_old = 1e6
+        tolerance = self.params_adjoint.adjoint_tolerance
+        max_iterations = self.params_adjoint.adjoint_max_iterations
+        iteration = 0
+        optimize_face = self.params_adjoint.adjoint_optimize_face
+        self.finite_difference_ss.solve()
+        current_T = self.finite_difference_ss.get_temperature_face(target_face)
+        lamb = np.zeros_like(self.finite_difference_ss.T)
+        step_size = self.params.adjoint_step_size
+        moment = 0
+        moment_2 = 0
+        beta = 0
+        beta_2 = 0
+
+        # Loop until target found
+        while error > tolerance and iteration < max_iterations:
+
+            # Calculate the dirichlet boundary condition
+            adjoint_rhs = current_T - self.target_T
+
+            # Solve the adjoint problem
+            self.adjoint = copy.deepcopy(self.finite_difference_ss)
+            self.adjoint.boundary.set_rhs(target_face, self.adjoint.rhs, -adjoint_rhs)
+            # self.adjoint.boundary.set_A(target_face, self.adjoint.A, 1.0)
+            self.adjoint.solve(transpose=True)
+
+            # Store lambda transpose and apply the optimization step
+            lamb = self.adjoint.get_temperature_face(optimize_face, values = self.adjoint.T)
+
+            # Calculate gradient
+            old_h = self.finite_difference_ss.boundary.get_convective_coefficient_at_face(optimize_face)
+            
+            gradient = -lamb * (current_T - float(self.params.T_inf[target_face]))
+
+            # Calculate momentum
+            
+            moment = beta * moment + (1 - beta) * gradient
+            moment_2 = beta_2 * moment_2 + (1 - beta_2) * np.square(gradient)
+
+            # Apply newton method
+            new_h = old_h + 10* moment / np.sqrt(moment_2 + 1e-8)
+
+            # Apply constraint
+            new_h = np.maximum(new_h, 0.0)
+
+            # Apply new convection coefficient
+            self.finite_difference_ss.boundary.set_convective_heat_transfer_map_for_face(optimize_face, new_h)
+
+            # Run steady state simulation
+            self.finite_difference_ss.solve()
+            if self.params_adjoint.save_output:
+                self.data_manager.save_vtu(1, self.finite_difference_ss.T, self.finite_difference_ss.heat_flux)
+                self.data_manager.generate_pvd(1)
+
+            # Calculate error as RMSE
+            new_T = self.finite_difference_ss.get_temperature_face(target_face)
+            error = np.linalg.norm(new_T - self.target_T, 2)/np.sqrt(len(new_T))
+            current_T = new_T
+
+            # Print logs
+            print(f'Iteration: {iteration}, Error: {error:.4f}')
+
+            # Save adjoint output
+            if self.params.adjoint_output_results:
+                self.save_adjoint_output(iteration, error, new_h, current_T, "adjoint_output")
+
+            beta = 0.9
+            beta_2 = 0.999
+
+            iteration += 1
+
+        print(f'Iterations: {iteration}')
+
+
+    def save_adjoint_output(self, iteration, error, convective_coefficient, T, output_folder):
+
+        # Create output folder if it doesn't exist
+        os.makedirs(output_folder, exist_ok=True)
+
+        file_name = f"{output_folder}/adjoint.csv"
+        h_conv_file_name = f"{output_folder}/adjoint_h_conv.csv"
+        T_file_name = f"{output_folder}/adjoint_T.csv"
+        
+        # If the file doesn't exist, create it with headers
+        if iteration == 0 or not os.path.exists(file_name):
+            with open(file_name, 'w', newline="") as f:
+                f.write("iteration, error\n")
+
+        else:
+            with open(file_name, 'a') as f:
+                writer = csv.writer(f, delimiter=",")
+                formatted_data = [iteration, f"{error:.4f}"]
+                writer.writerow(formatted_data)
+
+        # If the file doesn't exist, create it with headers
+        if iteration == 0 or not os.path.exists(h_conv_file_name):
+            with open(h_conv_file_name, 'w') as f:
+                f.write("")
+
+        else:
+            with open(h_conv_file_name, 'a') as f:
+                writer = csv.writer(f)
+                formatted_data = [f"{h:.4f}" for h in convective_coefficient]
+                writer.writerow(formatted_data)
+
+        # If the file doesn't exist, create it with headers
+        if iteration == 0 or not os.path.exists(T_file_name):
+            with open(T_file_name, 'w') as f:
+                f.write("")
+
+        else:
+            with open(T_file_name, 'a') as f:
+                writer = csv.writer(f)
+                formatted_data = [f"{t:.4f}" for t in T]
+                writer.writerow(formatted_data)
 
 
