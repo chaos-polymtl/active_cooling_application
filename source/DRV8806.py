6d8ab75c5fc0cd6d7b74375daa5e194bf031118c
Wieb-ke <wiebke.mainville@polymtl.ca>
2026-03-23 17:35:44 -0400

    fix solenoid issue by changing from lgpio back to GPIO

--- source/DRV8806.py
+++ source/DRV8806.py
@@ -12,8 +12,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
-import lgpio
-_h = lgpio.gpiochip_open(4)
+import RPi.GPIO as GPIO
 
 import spidev
 import time
@@ -25,15 +24,14 @@ class DRV8806:
         self.srclr = srclr
         self.reset = reset
 
-        self.h = lgpio.gpiochip_open(0)
-        lgpio.gpio_claim_output(self.h, self.latch)
-        lgpio.gpio_claim_output(self.h, self.srclr)
-        lgpio.gpio_claim_output(self.h, self.reset)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.latch, GPIO.OUT)
+        GPIO.setup(self.srclr, GPIO.OUT)
+        GPIO.setup(self.reset, GPIO.OUT)
 
-
-        lgpio.gpio_write(self.h, self.latch, 1)
-        lgpio.gpio_write(self.h, self.srclr, 1)
-        lgpio.gpio_write(self.h, self.reset, 0)
+        GPIO.output(self.latch, GPIO.HIGH)
+        GPIO.output(self.srclr, GPIO.HIGH)
+        GPIO.output(self.reset, GPIO.LOW)
 
         self.spi = spidev.SpiDev()
         self.spi.open(bus, device)
@@ -42,6 +40,6 @@ class DRV8806:
         self.spi.mode = 0b00
 
     def reset_driver(self):
-        lgpio.gpio_write(self.h, self.reset, 1)
+        GPIO.output(self.reset, GPIO.HIGH)
         time.sleep(0.1)
-        lgpio.gpio_write(self.h, self.reset, 0)
+        GPIO.output(self.reset, GPIO.LOW)
